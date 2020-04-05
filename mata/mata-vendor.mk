44b99abe3cb8d730e0e18007e2fbce6889bfaee4
bder <2243168976@qq.com>
2020-04-05 12:04:43 +0000

    Add modify libsqlite

--- mata/mata-vendor.mk
+++ mata/mata-vendor.mk
@@ -805,6 +805,8 @@ PRODUCT_COPY_FILES += \
     vendor/essential/mata/proprietary/vendor/lib64/vendor.qti.imsrtpservice@1.0-service-Impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.imsrtpservice@1.0-service-Impl.so \
     vendor/essential/mata/proprietary/vendor/lib64/vendor.qti.imsrtpservice@1.0_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.imsrtpservice@1.0_vendor.so \
     vendor/essential/mata/proprietary/vendor/radio/qcril_database/qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db
+    vendor/essential/mata/proprietary/lib/libsqlite.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsqlite.so \
+    vendor/essential/mata/proprietary/lib64/libsqlite.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsqlite.so
 
 PRODUCT_PACKAGES += \
     libsdm-disp-vndapis \
